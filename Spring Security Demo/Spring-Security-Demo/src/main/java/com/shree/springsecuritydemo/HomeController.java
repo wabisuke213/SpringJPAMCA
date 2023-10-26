d2016fc27e33bfc7187cfb82f6e6a63b8998aac6
Shree Bhat <shreekara.bhat51@svkmmumbai.onmicrosoft.com>
2023-10-26 20:25:23 +0530

    COMMIT

---
+++ Spring Security Demo/Spring-Security-Demo/src/main/java/com/shree/springsecuritydemo/HomeController.java
@@ -0,0 +1,18 @@
+package com.shree.springsecuritydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "This is Home Page";
+    }
+
+    @GetMapping("/Admin")
+    public String Admin() {
+        return "This is Admin Page";
+    }
+}
